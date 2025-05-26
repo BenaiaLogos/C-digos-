982c71ca6a6765980c7f57890fcb7d4d713a8fae
Logos <misael.pinto@estudante.uffs.edu.br>
2025-05-26 09:24:15 -0300

    Adicionando soma de matriz

--- teste.py
+++ teste.py
@@ -1,14 +1,18 @@
 #Como cria matrizes
-def criar():
-     A = [] #Matriz vazia 
-     l,c = map(int,input("Insira a quantidade de linhas {l} e de {c} que você deseja").split())
-     for linhas in range(l):
-      New_matriz =[]
-      for colunas in range(c):
-        number = int(input("insira o número desejado"))
-        New_matriz.append(number)
-      A.append(New_matriz)
-     return A
-print(len(criar()))
-def multiplicacao(multium,multidois):
-   D = str(input(""))
+A = [[1,2],
+      [4,5]]
+B = [[2,3],
+     [5,6]]
+print(A[0][1])
+# A[linha][coluna], parecido com o conceito de branch 
+print(len(A)) # len é a quantidade de linhas
+print(len(A[0]))# é a quantidade de colunas, ou seja len  e acessando um número, descobrimos a quantidade de colunas
+Matriz_soma = []
+for linhas in range(len(A)):
+    l_Matriz_soma = []
+    for colunas in range(len(A[0])):
+        soma = A[linhas][colunas] + B[linhas][colunas]
+        l_Matriz_soma.append(soma)
+    Matriz_soma.append(l_Matriz_soma)
+print(Matriz_soma)
+
