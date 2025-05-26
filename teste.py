29bb48e5201d37a98602497d20ef32db597383ea
Logos <misael.pinto@estudante.uffs.edu.br>
2025-05-26 08:41:22 -0300

    como criar matrizes

--- teste.py
+++ teste.py
@@ -1,27 +1,14 @@
-
-while True:
-   N = int(input())
-   N = min(max(N,10),10**5)
-   S = str(input().split())
-   T = str(input().split())
-   Linguas = []
-   Letras = []
-   contador_um = 0
-   contador_dois = 0
-   for numeros in S:
-     if numeros is not numeros.isdigit():
-       Linguas.append(numeros)
-   for elementos in T:
-    if elementos is not elementos.isdigit():
-      Letras.append(elementos)
-   for astericos in Letras:
-     if astericos == "*":
-      contador_um+=1
-   for Astericos in Linguas:
-    if Astericos == "*":
-      contador_dois+=1
-   if contador_um > N or contador_dois> N:
-       continue
-   sub = contador_dois - contador_um
-   razao = ((f"{sub/contador_dois:.2f}"))
-   print(razao)
+#Como cria matrizes
+def criar():
+     A = [] #Matriz vazia 
+     l,c = map(int,input("Insira a quantidade de linhas {l} e de {c} que você deseja").split())
+     for linhas in range(l):
+      New_matriz =[]
+      for colunas in range(c):
+        number = int(input("insira o número desejado"))
+        New_matriz.append(number)
+      A.append(New_matriz)
+     return A
+print(len(criar()))
+def multiplicacao(multium,multidois):
+   D = str(input(""))
